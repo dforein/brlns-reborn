91137f7af24c92f3cb860f400b7713061138f748
dforein <dannyforeigner@gmail.com>
2026-07-26 23:02:25 +0200

    Fix: food updates when eating

--- src/main/java/org/brlnsreb/listeners/general/PlayerEatListener.java
+++ src/main/java/org/brlnsreb/listeners/general/PlayerEatListener.java
@@ -1,10 +1,5 @@
 package org.brlnsreb.listeners.general;
 
-import java.util.HashSet;
-import java.util.UUID;
-
-import org.brlnsreb.core.player.CustomPlayer;
-import org.brlnsreb.core.player.PlayerUtils;
 import org.powernukkitx.event.EventHandler;
 import org.powernukkitx.event.Listener;
 import org.powernukkitx.event.player.PlayerFoodLevelChangeEvent;
@@ -12,16 +7,10 @@ import org.powernukkitx.plugin.annotation.EventListener;
 
 @EventListener
 public class PlayerEatListener implements Listener {
-
-    private final HashSet<UUID> idSet = new HashSet<>();
     
     @EventHandler
     public void onPlayerFoodLevelChange(PlayerFoodLevelChangeEvent event) {
-        CustomPlayer player = (CustomPlayer) event.getPlayer();
-        if (idSet.remove(player.getUniqueId())) return;
-
-        idSet.add(player.getUniqueId());
-        PlayerUtils.setFood(player, 18);
+        event.setFoodLevel(18);
     }
 
 }
