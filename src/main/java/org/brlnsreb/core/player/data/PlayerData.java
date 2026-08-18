e8b371a94753dd6cb033151f7c0b6606174299a6
dforein <dannyforeigner@gmail.com>
2026-08-18 14:24:21 +0200

    Small refactoring

--- src/main/java/org/brlnsreb/core/player/data/PlayerData.java
+++ src/main/java/org/brlnsreb/core/player/data/PlayerData.java
@@ -85,6 +85,7 @@ public class PlayerData {
         synchronized (accountLock) { return this.coins; }
     }
 
+
     //exp and levels
 
     public void setExp(int exp) { 
@@ -95,27 +96,26 @@ public class PlayerData {
         synchronized (accountLock) { this.setExp(this.exp + deltaExp); }
     }
 
+    /**
+     * The level growth functions are based on values gathered from different yt videos, 
+     * at different levels (3-5, 40-60, 149, 7000+, 8000+), considering a more probable 
+     * use of exp boosters at higher level (=> higher player longevity) to correct some 
+     * weird data (i.e. abnormal level growth compared to the other values at lower levels)
+     */
     public void updateLevel() {
-        /**
-         * the level growth functions are based on values gathered from different yt videos, 
-         * at different levels (3-5, 40-60, 149, 7000+, 8000+), considering a more probable 
-         * use of exp boosters at higher level (=> higher player longevity) to correct some 
-         * weird data (i.e. abnormal level growth compared to the other values at lower levels)
-         */
         synchronized (accountLock) {
-            int expThreshold = 562500;          //equivalent to 150 levels
-            int levelThreshold = 150;
-            double expPerHighLevel = 7500.0;    //reciprocal of the derivative of the level function lvl(exp) at x=150
+            final int expThreshold = 562500;          //equivalent to 150 levels
+            final int levelThreshold = 150;
+            final double expPerHighLevel = 7500.0;    //reciprocal of the derivative of the level function lvl(exp) at x=150
 
             if (this.exp < expThreshold) {
                 this.level = Math.sqrt((double) this.exp) / 5.0;     //lvl(exp)
-                this.levelFloor = (int) this.level;
             } else {
                 // using the derivative of lvl(exp) to get a linear constant growth
                 int temp = this.exp - expThreshold;
                 this.level = levelThreshold + temp / expPerHighLevel;
-                this.levelFloor = (int) this.level;
             }
+            this.levelFloor = (int) this.level;
         }
     }
 
